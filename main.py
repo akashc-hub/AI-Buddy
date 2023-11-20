adf29d153cb7690f3666e3024e0b8d0be72bbbe7
Akash Chauhan <136882078+akashc-hub@users.noreply.github.com>
2023-11-20 21:07:42 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import speech_recognition as sr
 import os
 import win32com.client
 import webbrowser
+import datetime
+import winshell as winshell    # pip install winshell
+import pywhatkit         # pip install pywhatkit
+import pyautogui        # pip install pyautogui
+import pyjokes          # pip install pyjokes
 import openai
 def takeCommand():
     r = sr.Recognizer()
@@ -17,9 +22,9 @@ def takeCommand():
             return "Some Error Occured, Sorry!"
 
 if __name__ == '__main__':
-    print('Pycharm')
+    print('AI-Buddy')
     say = win32com.client.Dispatch("SAPI.SpVoice")
-    say.Speak("Jarvis A. I.")
+    say.Speak("A. I. Buddy")
     while True:
         print("Listening..")
         query = takeCommand()
@@ -33,4 +38,57 @@ if __name__ == '__main__':
             musicPath = "C:\\Users\\amd\\Desktop\\pythonProject\\Firecracker.mp3"
             os.system(f"start {musicPath}")
 
+        elif "tell the time" in query:
+            strfTime = datetime.datetime.now().strftime("%H:%M:%S")
+            say.Speak(f"Sir the time is {strfTime}")
+
+        elif "tell the date" in query:
+            strfDate = datetime.datetime.now().strftime("%Y-%m-%d")
+            say.Speak(f"Sir the Date is {strfDate}")
+
+        elif "open vs code" in query:
+            say.Speak("Opening VS code Sir...!")
+            os.startfile("C:\\Users\\amd\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe")
+
+        # elif 'search' in query:
+        #     query = query.replace('search', '')
+        #     pywhatkit.search(query)
+
+        elif 'empty recycle bin' in query or 'clear recycle bin' in query:
+            try:
+                winshell.recycle_bin().empty(confirm=False, show_progress=False, sound=True)
+                print("Recycle Bin is cleaned successfully.")
+                say.Speak("Recycle Bin is cleaned successfully.")
+            except Exception as e:
+                print("Recycle bin is already Empty.")
+                say.Speak("Recycle bin is already Empty.")
+
+        elif 'write a note' in query or 'make a note' in query:
+            say.Speak("What should I write, sir??")
+            note = takeCommand()
+            str_time = datetime.datetime.now().strftime("%H:%M:%S")
+            file = open('Notes.txt', 'a')
+            file.write(str_time)
+            file.write(" --> ")
+            file.write(note)
+            say.Speak("Point noted successfully.")
+
+        elif 'show me the notes' in query or 'read notes' in query:
+            say.Speak("Reading Notes")
+            file = open("Notes.txt", "r")
+            data_note = file.readlines()
+            # for points in data_note:
+            print(data_note)
+            say.Speak(data_note)
+
+        elif 'take screenshot' in query:
+            sc = pyautogui.screenshot()
+            sc.save('pa_ss.png')
+            print("Screenshot taken successfully.")
+            say.Speak("Screenshot taken successfully.")
+
+        elif 'tell me a joke' in query:
+            joke = pyjokes.get_joke()
+            print(joke)
+            say.Speak(joke)
         # say(query)
